0baef514e162c3a05926136290285c3009c59383
Bobby Galli <bobbyg603@gmail.com>
2018-09-08 16:34:36 -0400

    Renamed to litpay-web.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'litpay-client'`, async(() => {
+  it(`should have as title 'litpay-web'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('litpay-client');
+    expect(app.title).toEqual('litpay-web');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to litpay-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to litpay-web!');
   }));
 });
