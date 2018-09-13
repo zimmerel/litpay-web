e97bc4c12e93a360029be59ce08ebc6a74b0ec7d
Zach <zmriel@gmail.com>
2018-09-12 21:27:06 -0400

    Added login page with functionality, needs css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,19 @@ import { LoginComponent } from './authentication/components/login/login.componen
 import { RegisterComponent } from './authentication/components/register/register.component';
 import { AuthenticationService } from './authentication/services/authentication.service';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatInputModule,
+  MatIconModule
+} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+// import {  } from '@angular/material/form-field';
 
 const routes = [
-  { path: "", component: HomeComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent }
-]
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +31,12 @@ const routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+      '';
   }
 
 }
