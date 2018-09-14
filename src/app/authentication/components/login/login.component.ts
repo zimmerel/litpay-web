4c831c25dd9d1c3049243951d35070f618441465
Zach <zmriel@gmail.com>
2018-09-13 22:13:48 -0400

    Added login form components, with some validation

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  FormBuilder, FormGroup,
+  FormControl, Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,24 +10,31 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  hide = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  hidePassword = true;
 
-  constructor() { }
+  public registerForm: FormGroup = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
   getEmailErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' :
+    const emailFormControl = this.registerForm.controls['email'];
+    return emailFormControl.hasError('required') ? 'You must enter a value' :
+      emailFormControl.hasError('email') ? 'Not a valid email' :
         '';
   }
-
   getPasswordErrorMessage() {
-    return this.password.hasError('required') ? 'You must enter a value' :
-      '';
+    return this.registerForm.controls['password']
+    .hasError('required') ? 'You must enter a value' : '';
   }
 
+  onSubmit(): void {
+    console.log(this.registerForm.controls['email'].value);
+    console.log(this.registerForm.controls['password'].value);
+  }
 }
